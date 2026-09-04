b21b553ad620d687d4e254b578fb1e80f25fb712
slimanov <slimanov@diasoft.ru>
2026-09-04 10:59:12 +0300

    4. Особенности языка и типовые ошибки

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,94 @@
-package hw02unpackstring
+
+package hw02_unpack_string
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
+
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+
+func Unpack(s string) (string, error) {
+
+	var b strings.Builder
+
+	var runes []rune = []rune(s)
+
+
+	var prev rune
+	var hasPrev bool = false
+
+
+	var i int = 0
+	for i < len(runes) {
+		var r rune = runes[i]
+
+
+		if r == '\\' {
+
+			if i+1 >= len(runes) {
+				return "", ErrInvalidString
+			}
+			var next rune = runes[i+1]
+
+			if !(unicode.IsDigit(next) || next == '\\') {
+				return "", ErrInvalidString
+			}
+
+
+			if hasPrev {
+				b.WriteRune(prev)
+				hasPrev = false
+			}
+
+			prev = next
+			hasPrev = true
+
+
+			i += 2
+			continue
+		}
+
+		//
+		if unicode.IsDigit(r) {
+
+			if !hasPrev {
+				return "", ErrInvalidString
+			}
+
+
+			var n int
+			n, _ = strconv.Atoi(string(r))
+
+
+			if n > 0 {
+				b.WriteString(strings.Repeat(string(prev), n))
+			}
+
+			hasPrev = false
+			i++
+			continue
+		}
+
+
+		if hasPrev {
+			b.WriteRune(prev)
+		}
+		// Текущий символ становится новым "ожидающим".
+		prev = r
+		hasPrev = true
+		i++
+	}
+
+
+	if hasPrev {
+		b.WriteRune(prev)
+	}
+
+
+	return b.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -1,4 +1,4 @@
-package hw02unpackstring
+package hw02_unpack_string
 
 import (
 	"errors"
@@ -17,10 +17,15 @@ func TestUnpack(t *testing.T) {
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		// дополнительные кейсы
+        {input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+        {input: "a0", expected: ""},
+        {input: "a1", expected: "a"},
+        {input: `\1`, expected: `1`},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +39,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", `qwe\`, `a\b`}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
